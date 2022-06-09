e5ae12ada6874d01ae1cf5d66ec7f6ebe4986c8f
Lin <1725525653@qq.com>
2022-06-09 22:28:10 +0800

    BUG FIXED 中文乱码问题

--- client/src/api/system/feedback.js
+++ client/src/api/system/feedback.js
@@ -4,6 +4,7 @@
  * @Date: 2022-06-05 17:22:40
  */
 import request from '@/utils/request';
+import qs from 'qs';
 
 export function getList(params) {
   return request({
@@ -23,6 +24,6 @@ export function reply(params) {
   return request({
     url: '/feedback/reply',
     method: 'put',
-    params,
+    data: qs.stringify(params),
   });
 }
